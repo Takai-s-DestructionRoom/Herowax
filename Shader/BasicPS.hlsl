8add1db5a4cf2f6d035acc0e8cd7106e344f254e
beluga <belugagit5719@gmail.com>
2024-03-05 11:53:23 +0900

    ディザ抜き入れた

--- Shader/BasicPS.hlsl
+++ Shader/BasicPS.hlsl
@@ -3,8 +3,36 @@
 Texture2D<float4> tex : register(t0); //0番スロットに設定されたテクスチャ
 SamplerState smp : register(s0); //0番スロットに設定されたサンプラー
 
+//ディザ抜きの目の細かさ(本来は外部テクスチャから読み込んだ方がいい)
+static const int pattern[16] =
+{
+    0, 8, 2, 10,
+    12, 4, 14, 6,
+    3, 11, 1, 9,
+    15, 7, 13, 5
+};
+
+float3 magnitude(float3 v)
+{
+    return (v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
 float4 main(VSOutput input) : SV_TARGET
 {
+    //----------ディザ抜きの諸々の処理-------------//
+    int ditherUV_x = (int) fmod(input.svpos.x, 4.0f); //パターンの大きさで割ったときの余りを求める
+    int ditherUV_y = (int) fmod(input.svpos.y, 4.0f); //今回のパターンサイズは4x4なので4で除算
+    float dither = pattern[ditherUV_x + ditherUV_y * 4]; //求めた余りからパターン値を取得
+    
+    float camlength = magnitude(cameraPos - input.wpos.xyz);
+    
+    float clamp_dither = saturate(dither / 4);
+    float clamp_length = saturate(camlength / 50);
+    
+    clip(clamp_length - clamp_dither); //閾値が0以下なら描画しない
+    
+    //----------ディザ抜きの諸々の処理終わり----------//
+    
 	float4 texcolor = float4(tex.Sample(smp, input.uv));
 	texcolor = texcolor * m_color;
 	
