4178de6b6d9b470626113b95fb20367d69b51ca2
晴彦 相川 <k021g1126@g.neec.ac.jp>
2024-03-20 18:02:47 +0900

    テクスチャそれっぽくなった。けどシェーダーもいじりたいので一旦プッシュ。

--- Shader/ParticleBillboard/ParticleBillboardPS.hlsl
+++ Shader/ParticleBillboard/ParticleBillboardPS.hlsl
@@ -8,6 +8,13 @@ float4 main(GSOutput input) : SV_TARGET
     // テクスチャマッピング
     float4 texcolor = tex.Sample(smp, input.uv);
     
+    float threshold = 0.3;
+
+    if (texcolor.r < threshold || texcolor.g < threshold || texcolor.b < threshold)
+    {
+        texcolor.a = 0.0;
+    }
+    
     // シェーディング色で描画
     float4 color = texcolor * input.color;
     
