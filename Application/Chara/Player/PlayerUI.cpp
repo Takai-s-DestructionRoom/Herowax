d7bfe7150f94b1dda02100a689b2b21f65492758
晴彦 相川 <k021g1126@g.neec.ac.jp>
2024-03-27 11:24:28 +0900

    ロウをゲージ性にするか切り替えられるように。XボタンかQキーでロウ回収機能追加。

--- Application/Chara/Player/PlayerUI.cpp
+++ Application/Chara/Player/PlayerUI.cpp
@@ -8,23 +8,44 @@ void PlayerUI::LoadResource()
 	TextureManager::Load("./Resources/white2x2.png", "white2x2");
 }
 
-PlayerUI::PlayerUI() : position(1, 1, 1), size(5.f, 0.2f)
+PlayerUI::PlayerUI()
 {
+	//色指定
+	gaugeColor[(uint32_t)UIType::waxGauge] = Color::kWhite;
+	gaugeColor[(uint32_t)UIType::fireGauge] = Color::kRed;
+
+	//サイズ指定
+	for (uint32_t i = 0; i < (uint32_t)UIType::max; i++)
+	{
+		size[i] = { 5.f,0.2f };
+		maxSize[i] = size[i] * 1.1f;
+	}
 }
 
 void PlayerUI::Update(Player* player)
 {
-	position = player->obj.mTransform.position;
-	position.y += player->GetScale().y * 1.8f;
-	size.x = player->fireUnit.fireGauge;
-	maxSize.x = player->fireUnit.maxFireGauge * 1.1f;
-	maxSize.y = size.y * 1.1f;
+	//座標を合わせ続ける
+	for (uint32_t i = 0; i < (uint32_t)UIType::max; i++)
+	{
+		position[i] = player->obj.mTransform.position;
+		position[i].y += player->GetScale().y * 4.f + (float)(i) * 0.5f;
+	}
+
+	//ゲージの割合を指定されたサイズとかける
+	size[(uint32_t)UIType::waxGauge].x =
+		maxSize[(uint32_t)UIType::waxGauge].x * ((float)player->waxStock / (float)player->maxWaxStock);
+
+	size[(uint32_t)UIType::fireGauge].x =
+		maxSize[(uint32_t)UIType::fireGauge].x * player->fireUnit.fireGauge / player->fireUnit.maxFireGauge;
 }
 
 void PlayerUI::Draw()
 {
-	Vector3 backPos = position;
-	backPos.z += 0.01f;
-	InstantDrawer::DrawGraph3D(backPos, maxSize.x, maxSize.y, "white2x2", Color::kBlack);
-	InstantDrawer::DrawGraph3D(position, size.x, size.y, "white2x2", Color::kRed);
+	for (uint32_t i = 0; i < (uint32_t)UIType::max; i++)
+	{
+		Vector3 backPos = position[i];
+		backPos.z += 0.01f;
+		InstantDrawer::DrawGraph3D(backPos, maxSize[i].x, maxSize[i].y, "white2x2", Color::kBlack);
+		InstantDrawer::DrawGraph3D(position[i], size[i].x, size[i].y, "white2x2", gaugeColor[i]);
+	}
 }
