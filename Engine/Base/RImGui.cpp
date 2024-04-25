91ed546e4ba3ce3127d39dceb8624981d379c110
beluga <belugagit5719@gmail.com>
2024-04-25 13:39:06 +0900

    デバッグモードに入るコード部分をすべてコメントアウト

--- Engine/Base/RImGui.cpp
+++ Engine/Base/RImGui.cpp
@@ -28,9 +28,9 @@ void RImGui::NewFrame()
 
 void RImGui::Render()
 {
-    if (RInput::GetKeyDown(DIK_F7)) {
+  /*  if (RInput::GetKeyDown(DIK_F7)) {
         showImGui = !showImGui;
-    }
+    }*/
 
     //if (!showImGui)return;
 
