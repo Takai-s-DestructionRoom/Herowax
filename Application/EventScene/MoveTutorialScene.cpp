177d3f22c8311abb490681e1bcdd0253cb7f6fa3
beluga <belugagit5719@gmail.com>
2024-05-31 21:48:45 +0900

    移動&カメラのチュートリアルを適当に作成

--- Application/EventScene/MoveTutorialScene.cpp
+++ Application/EventScene/MoveTutorialScene.cpp
@@ -1,11 +1,11 @@
 #include "MoveTutorialScene.h"
 #include "Player.h"
 #include "InstantDrawer.h"
+#include "GameCamera.h"
 
 MoveTutorialScene::MoveTutorialScene()
 {
-	moveCountTimer = 0.2f;
-	cameraMoveCountTimer = 0.2f;
+
 }
 
 MoveTutorialScene::~MoveTutorialScene()
@@ -18,7 +18,7 @@ void MoveTutorialScene::Init(const Vector3 target)
 	camera.mViewProjection.mTarget = target;
 	camera.mViewProjection.UpdateMatrix();
 
-	Camera::sNowCamera = &camera;
+	Camera::sNowCamera = &GameCamera::GetInstance()->camera;
 
 	eventTimer.Start();
 
@@ -27,31 +27,58 @@ void MoveTutorialScene::Init(const Vector3 target)
 	tutorialUIPos = { RWindow::GetWidth() * 0.5f,RWindow::GetHeight() * 0.5f - 100.f };
 	tutorialUIPos.y += 300.f;
 
-	moveCountTimer.Start();
-	cameraMoveCountTimer.Start();
+	moveCountGauge.Init("moveCountGauge");
+	cameraCountGauge.Init("cameraCountGauge");
+
+	moveCountGauge.baseRadian = 360.f;
+	cameraCountGauge.baseRadian = 360.f;
+
+	moveOk = TextureManager::Load("./Resources/UI/ok.png", "ok");
+	cameraOk = TextureManager::Load("./Resources/UI/ok.png", "ok");
 }
 
 void MoveTutorialScene::Update()
 {
 	eventTimer.Update();
 
-	camera.mViewProjection.UpdateMatrix();
+	GameCamera::GetInstance()->Update();
 
 	//動いてる間
 	if (Player::GetIsMove())
 	{
-		moveCountTimer.Update();
+		moveCountGauge.baseRadian -= 2.0f;
 	}
 	//カメラ動かしてる間
 	if (Player::GetIsCameraMove())
 	{
-		cameraMoveCountTimer.Update();
+		cameraCountGauge.baseRadian -= 3.0f;
 	}
 
-	if (moveCountTimer.GetEnd() && cameraMoveCountTimer.GetEnd()) {
+	moveCountGauge.baseRadian = Util::Clamp(moveCountGauge.baseRadian, 0.0f, 360.f);
+	cameraCountGauge.baseRadian = Util::Clamp(cameraCountGauge.baseRadian, 0.0f, 360.f);
+
+	if (moveCountGauge.baseRadian <= 0.0f && cameraCountGauge.baseRadian <= 0.0f) {
 		Camera::sNowCamera = nullptr;
 		isActive = false;
 	}
+
+	moveCountGauge.ImGui();
+	cameraCountGauge.ImGui();
+
+	moveCountGauge.Update();
+	cameraCountGauge.Update();
+
+	moveOk.mTransform.position = moveCountGauge.baseTrans.position;
+	moveOk.mTransform.position.x += 100;
+
+	moveOk.mTransform.UpdateMatrix();
+	moveOk.TransferBuffer();
+	
+	cameraOk.mTransform.position = cameraCountGauge.baseTrans.position;
+	cameraOk.mTransform.position.x += 100;
+
+	cameraOk.mTransform.UpdateMatrix();
+	cameraOk.TransferBuffer();
 }
 
 void MoveTutorialScene::Draw()
@@ -59,4 +86,14 @@ void MoveTutorialScene::Draw()
 	InstantDrawer::DrawGraph(
 		tutorialUIPos.x, tutorialUIPos.y,
 		0.5f, 0.5f, 0.f, TextureManager::Load("./Resources/UI/tutorial_move.png", "tutorial_move"));
+
+	moveCountGauge.Draw();
+	cameraCountGauge.Draw();
+	
+	if (moveCountGauge.baseRadian <= 0.0f) {
+		moveOk.Draw();
+	}
+	if (cameraCountGauge.baseRadian <= 0.0f) {
+		cameraOk.Draw();
+	}
 }
--- Application/EventScene/MoveTutorialScene.h
+++ Application/EventScene/MoveTutorialScene.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "IEventScene.h"
 #include "Vector2.h"
+#include "CircleGauge.h"
 
 class MoveTutorialScene : public IEventScene
 {
 private:
 	Vector2 tutorialUIPos{};
 
-	Easing::EaseTimer moveCountTimer;			//移動時間測る用
-	Easing::EaseTimer cameraMoveCountTimer;		//カメラ移動時間測る用
+	CircleGauge moveCountGauge;		//動かしたらゲージが伸びて
+	CircleGauge cameraCountGauge;	
+
+	Sprite moveOk;		//okって出したい
+	Sprite cameraOk;	
 
 public:
 	MoveTutorialScene();
